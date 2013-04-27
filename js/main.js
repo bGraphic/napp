5756018830e42f26f4b000e1f9e16a193f915ba0
Benedicte Raae <raae@bgraphic.no>
2013-04-27 14:23:00 +0200

    Popover for list information

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
-
-function hideSeafoodCollectionSpinner() {
-    $("#seafood-spinner").hide();
-}
-
-
 $(function() {
+
+    var listInfoGreen = "Disse artene høstes på en bærekraftig måte og kan spises med god samvittighet.";
+    var listInfoOrange = "Disse artene høstes stort sett på en bærekraftig måte. Fangstmetoder og områder kan være av betydning for miljøvennlighet.";
+    var listInfoRed = "Disse artene er sårbare for populasjonsreduksjoner eller høstes på en lite bærekraftig måte.";
+
+    function hideSeafoodCollectionSpinner() {
+        $("#seafood-spinner").hide();
+    }
+
+    $('body').popover({
+        selector: '[data-toggle="popover"]',
+        html: true,
+        placement: 'top'
+    });
 
     Parse.$ = jQuery;
 
@@ -65,11 +73,11 @@ $(function() {
 
             var status = this.model.get("status");
             if(status == "green")
-                this.model.set("status_text", name + " er på den <a class='green'>grønne</a> listen.");
+                this.model.set("status_text", name + ' er på den <a class="list-info green" data-toggle="popover" title="Grønn liste" data-content="<small></a>' + listInfoGreen + '</small>"> grønne listen.</a>');
             else if (status == "orange")
-                this.model.set("status_text", name + " er på den <a class='orange'>orange</a> listen.");
+                this.model.set("status_text", name + ' er på den <a class="list-info orange" data-toggle="popover" title="Orange liste" data-content="<small>' + listInfoOrange + '</small>"> orange listen</a>.');
             else if (status == "red")
-                this.model.set("status_text", name + " er på den <a class='red'>røde listen.</a>");
+                this.model.set("status_text", name + ' er på den <a class="list-info red" data-toggle="popover" title="Rød liste" data-content="<small>' + listInfoRed + '</small>"> røde listen.</a>');
             else
                 this.model.set("status_text", "Mangler listeklassifisering " + name);
 
