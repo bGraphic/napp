e2dd6bd91a0e7ee8c497255b9372d079d30a86ae
Benedicte Raae <raae@bgraphic.no>
2013-04-27 21:22:53 +0200

    Divided into more templates--ammend

--- js/views/seafood-info.js
+++ js/views/seafood-info.js
@@ -1,7 +1,70 @@
+
 var SeafoodStatusInfoView = Parse.View.extend({
     tagName: "div",
 
-    template: _.template($('#seafoodStatusRedInfoTemplate').html()),
+    initialize: function() {
+
+        this.configureTemplate(this.options.model.get("status"));
+    },
+
+    configureTemplate: function(status) {
+        if(status == "green")
+            this.template = _.template($('#seafoodStatusGreenInfoTemplate').html());
+        else if(status == "orange")
+            this.template = _.template($('#seafoodStatusOrangeInfoTemplate').html());
+        else
+            this.template = _.template($('#seafoodStatusRedInfoTemplate').html());
+    },
+
+    render: function () {
+        this.$el.html(this.template(this.model.toJSON()));
+        return this;
+    }
+
+});
+
+var SeafoodEnvStatusInfoView = Parse.View.extend({
+    tagName: "div",
+
+    initialize: function() {
+
+        this.configureTemplate(this.options.model.get("env_status"));
+    },
+
+    configureTemplate: function(status) {
+        if(status == "smil")
+            this.template = _.template($('#seafoodEnvStatusGoodInfoTemplate').html());
+        else if(status == "ok")
+            this.template = _.template($('#seafoodEnvStatusOkInfoTemplate').html());
+        else if(status == "sur")
+            this.template = _.template($('#seafoodEnvStatusBadInfoTemplate').html());
+        else
+            this.template = _.template($('#seafoodEnvStatusNoneInfoTemplate').html());
+    },
+
+    render: function () {
+        this.$el.html(this.template(this.model.toJSON()));
+        return this;
+    }
+
+});
+
+var SeafoodEnvInfoView = Parse.View.extend({
+    tagName: "div",
+
+    initialize: function() {
+
+        this.configureTemplate(this.options.model.get("status"));
+    },
+
+    configureTemplate: function(status) {
+        if(status == "green")
+            this.template = _.template($('#seafoodStatusGreenInfoTemplate').html());
+        else if(status == "orange")
+            this.template = _.template($('#seafoodStatusOrangeInfoTemplate').html());
+        else
+            this.template = _.template($('#seafoodStatusRedInfoTemplate').html());
+    },
 
     render: function () {
         this.$el.html(this.template(this.model.toJSON()));
@@ -20,13 +83,23 @@ var SeafoodInfoView = Parse.View.extend({
 
         this.$el.html(this.template(this.model.toJSON()));
 
-        if(this.model.has("status")) {
-            var statusView = new SeafoodStatusInfoView({
-                model: this.model
-            });
 
-            this.$el.append(statusView.render().el);
-        }
+        var statusView = new SeafoodStatusInfoView({
+            model: this.model
+        });
+
+        statusView.model = this.model;
+
+        this.$el.append(statusView.render().el);
+
+        var envStatusView = new SeafoodEnvStatusInfoView({
+            model: this.model
+        });
+
+        envStatusView.model = this.model;
+
+        this.$el.append(envStatusView.render().el);
+
 
         return this;
     }
