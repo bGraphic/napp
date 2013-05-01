2f6259d8758579262d8c8640f6e6e2b8369f7549
Benedicte Raae <raae@bgraphic.no>
2013-05-01 14:10:25 +0200

    New env status template

--- js/views/seafood-info.js
+++ js/views/seafood-info.js
@@ -1,31 +1,6 @@
-
-var SeafoodStatusInfoView = Parse.View.extend({
-    tagName: "div",
-
-    initialize: function() {
-
-        this.configureTemplate(this.options.model.get("status"));
-    },
-
-    configureTemplate: function(status) {
-        if(status == "green")
-            this.template = _.template($('#seafoodStatusGreenInfoTemplate').html());
-        else if(status == "orange")
-            this.template = _.template($('#seafoodStatusOrangeInfoTemplate').html());
-        else
-            this.template = _.template($('#seafoodStatusRedInfoTemplate').html());
-    },
-
-    render: function () {
-        this.$el.html(this.template(this.model.toJSON()));
-        return this;
-    }
-
-});
-
 var SeafoodEnvStatusInfoView = Parse.View.extend({
     tagName: "div",
-    className: "badges",
+    className: "env-status",
 
     initialize: function() {
 
@@ -33,18 +8,16 @@ var SeafoodEnvStatusInfoView = Parse.View.extend({
     },
 
     configureTemplate: function(status) {
-        if(status == "smil")
-            this.template = _.template($('#seafoodEnvStatusGoodInfoTemplate').html());
-        else if(status == "ok")
-            this.template = _.template($('#seafoodEnvStatusOkInfoTemplate').html());
-        else if(status == "sur")
-            this.template = _.template($('#seafoodEnvStatusBadInfoTemplate').html());
+        if(status)
+            this.template = _.template($('#seafoodEnvStatusInfoTemplate').html());
         else
             this.template = _.template($('#seafoodEnvStatusNoneInfoTemplate').html());
     },
 
     render: function () {
+
         this.$el.html(this.template(this.model.toJSON()));
+
         return this;
     }
 
@@ -90,20 +63,9 @@ var SeafoodInfoView = Parse.View.extend({
 
         this.$el.html(this.template(this.model.toJSON()));
 
-
-        var statusView = new SeafoodStatusInfoView({
-            model: this.model
-        });
-
-        statusView.model = this.model;
-
-        this.$el.append(statusView.render().el);
-
         var envStatusView = new SeafoodEnvStatusInfoView({
             model: this.model
         });
-
-        envStatusView.model = this.model;
 
         this.$el.append(envStatusView.render().el);
 
@@ -111,15 +73,11 @@ var SeafoodInfoView = Parse.View.extend({
             model: this.model
         });
 
-        badgesView.model = this.model;
-
         this.$el.append(badgesView.render().el);
 
         var pictureView = new SeafoodPictureView({
             model: this.model
         });
-
-        pictureView.model = this.model;
 
         this.$el.append(pictureView.render().el);
 
